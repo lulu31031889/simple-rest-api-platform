70f8d2ccdd432244cc1f675c1e54f76471f68b49
lulu31031889 <lulu31031889@gmail.com>
2021-05-04 18:28:12 +0100

    Add Moq.  Add test for parameterless GetRandomInteger endpoint.

--- test/SimpleRestApiPlatform.Tests/Controllers/RandomNumbersTests.cs
+++ test/SimpleRestApiPlatform.Tests/Controllers/RandomNumbersTests.cs
@@ -1,17 +1,47 @@
-﻿using System;
+﻿using SimpleRestApiPlatform.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using Moq;
+using SimpleRestApiPlatform.Core.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace SimpleRestApiPlatform.Tests.Controllers
 {
     public class RandomNumbersTests
     {
+        readonly Mock<IRandomNumberGeneratorService> _mockIRandomNumberGeneratorService;
+
+        public RandomNumbersTests()
+        {
+            _mockIRandomNumberGeneratorService = new Mock<IRandomNumberGeneratorService>();
+        }
+
         [Fact]
         public void When_parameterless_GetRandomInteger_is_called()
-        { }
+        {
+            //Arrange
+            var r = new Random();
+            var randomNumber = r.Next();
+
+            _mockIRandomNumberGeneratorService
+                .Setup(x => x.GenerateRandomSigned32BitInteger(It.IsAny<int>(), It.IsAny<int>()))
+                .Returns(randomNumber);
+
+            var controller = new RandomNumbersController(_mockIRandomNumberGeneratorService.Object);
+
+            //Act
+            var result = controller.GetRandomInteger();
+
+            //Assert
+            Assert.IsType<OkObjectResult>(result);
+            Assert.True(((OkObjectResult)result).StatusCode == 200);
+            Assert.True(int.TryParse(((OkObjectResult)result).Value.ToString(), out var parsedValue));
+            Assert.True(parsedValue == randomNumber);
+        }
 
         [Fact]
         public void When_GetRandomInteger_is_called_with_2_strings_that_are_within_range_integers_but_min_is_greater_than_max()
